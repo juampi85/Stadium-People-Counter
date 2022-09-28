7ae503243748e9fb4c810daf16f46ed13c40bca1
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2022-09-28 13:11:28 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let count = 0;
+let countSection = document.getElementById("count-section");
+let saveSection = document.getElementById("save-section");
+let saveBtn = document.getElementById("save-btn");
+
+function increment(){
+    count++;
+    countSection.textContent = count;
+    saveBtn.removeAttribute('disabled');
+}
+
+function save(){
+    let record = count + " - ";
+    saveSection.textContent += record;
+    count = 0;
+    countSection.textContent = 0;
+    saveBtn.setAttribute('disabled', "true");
+}
+
+function restart(){
+    count = 0;
+    countSection.textContent = 0;
+    record = "";
+    saveSection.textContent = "Previous entires: ";
+    saveBtn.setAttribute('disabled', "true");
+}
